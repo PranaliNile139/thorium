3ed7d08462a905e26fbf5b7e46745e8f7402ca56
PranaliNile139 <pranalinile1391995@gmail.com>
2022-03-10 00:32:40 +0530

    Assignment

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,31 +1,21 @@
 const jwt = require("jsonwebtoken");
 
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
-    let token = req.headers["x-Auth-token"];
-    if (!token) token = req.headers["x-auth-token"];
-    if (!token) return res.send({ status: false, msg: "token must be present" });
-    // console.log(token);
+let authenticate= function (req,res,next){
+let token = req.headers["x-Auth-token"];
+if (!token) token = req.headers["x-auth-token"];
+if (!token) return res.send({ status: false, msg: "token must be present" });
+else {next()}
+}
 
-    next()
+let authorise= function (req,res,next){
+    let token= req.headers["x-auth-token"];
+    let decodedToken= jwt.verify(token, "functionup-thorium")
+    let toBeUpdatedUserId= req.params.userId;
+    let loggedInUserId= decodedToken.userId;
+    if(loggedInUserId != toBeUpdatedUserId) return res.send({status:false, msg:"you are not authorized to perform this task"})
+    else {next()}
 }
 
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    let token = req.headers["x-auth-token"];
-    let decodedToken = jwt.verify(token, 'functionup-thorium')
-    // if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
-
-    let userToBeModified = req.params.userId
-    let userLoggedIn = decodedToken.userId
-
-    if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
-
-   
-    next()
-}
-
-module.exports.authenticate = authenticate;
-module.exports.authorise = authorise
+module.exports.authenticate= authenticate
+module.exports.authorise= authorise
